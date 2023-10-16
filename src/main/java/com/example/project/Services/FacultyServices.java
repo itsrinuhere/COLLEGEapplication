78ea432610b16786718d824c2f93c7156b065c7a
itsrinuhere121 <itsrinuhere1@gmail.com>
2023-10-17 01:20:08 +0530

    updated SQl and Endpoints

--- src/main/java/com/example/project/Services/FacultyServices.java
+++ src/main/java/com/example/project/Services/FacultyServices.java
@@ -1,18 +1,16 @@
 package com.example.project.Services;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
 import java.util.*;
-
 @Service
 public class FacultyServices {
     @Autowired
     JdbcTemplate jt;
-    public int saveFaculty(HashMap<String,Object> map){
-//        Set<String> keySet = map.keySet();
-        if(isFacutlyInTable((String)map.get("userid"))) return -1;
+    public int saveFaculty(@NotNull HashMap<String,Object> map){
+        if(isFacultyInTable((String)map.get("userid"))) return -1;
         String sql="insert into faculty(userid,name,gender,dob,department,contactnumber,address,designation) values (?,?,?,?,?,?,?,?)";
         return jt.update(sql,
                 map.get("userid"),map.get("name"),map.get("gender"),
@@ -26,12 +24,48 @@ public class FacultyServices {
        }
        return dataS;
     }//working
-    public boolean isFacutlyInTable(String id){
+    public boolean isFacultyInTable(String id){
      try{
          jt.queryForMap("select userid from faculty where userid=?;",id);
          return true;
      }catch(Exception e){
          return false;
         }
+    }
+    public boolean isFacultyInexperineceTable(String id){
+        try{
+            jt.queryForMap("select userid from experience where userid=?;",id);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
+    public int saveExperience(@NotNull HashMap<String,Object> map){
+            if(isFacultyInexperineceTable((String)map.get("userid"))) return -1;
+            String ex =(String) map.get("exfrom");
+            String to = (String) map.get("exto");
+        String sql = "insert into experience(experienceid, userid, experiencetype, exfrom, exto, company, designation)" +
+                "values(?,?,?,'"+ex+"','"+to+"',?,?)";
+        return jt.update(sql,map.get("experienceid"),map.get("userid"),map.get("experiencetype"),map.get("company"),map.get("designation"));
+    }//completed
+    public int saveSocialLinks(HashMap<String,Object> map){
+        String ex =(String) map.get("exfrom");
+        String to = (String) map.get("exto");
+        String sql = "insert into experience(experienceid, userid, experiencetype, exfrom, exto, company, designation)" +
+                "values(?,?,?,'"+ex+"','"+to+"',?,?)";
+
+        return jt.update(sql,map.get("experienceid"),map.get("userid"),map.get("experiencetype"),map.get("company"),map.get("designation"));
+
+
+    }//SQL need to updated
+    public int saveResearchPapers(HashMap<String,Object> map){
+        String ex =(String) map.get("exfrom");
+        String to = (String) map.get("exto");
+        String sql = "insert into experience(experienceid, userid, experiencetype, exfrom, exto, company, designation)" +
+                "values(?,?,?,'"+ex+"','"+to+"',?,?)";
+
+        return jt.update(sql,map.get("experienceid"),map.get("userid"),map.get("experiencetype"),map.get("company"),map.get("designation"));
+
+        //throws
     }
 }
--- src/main/java/com/example/project/controller/FacultyController.java
+++ src/main/java/com/example/project/controller/FacultyController.java
@@ -1,12 +1,10 @@
 package com.example.project.controller;
 
 import com.example.project.Services.FacultyServices;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.Set;
 
@@ -18,6 +16,29 @@ public class FacultyController {
     @PostMapping("faculty")
     ResponseEntity<Object> saveFaculty(@RequestBody HashMap<String,Object> data){
         return new ResponseEntity<>(fs.saveFaculty(data)==1?HttpStatus.CREATED:HttpStatus.NO_CONTENT);
+    }
+    @PostMapping("faculty/{id}")
+    ResponseEntity<Object> saveSubFaculty(@RequestBody HashMap<String,Object> map, @PathVariable String id){
+       int res = -1;
+       System.out.println(map.get("userid"));
+        try{
+            switch (id) {
+                case "social" -> {
+                    res = fs.saveSocialLinks(map);
+                }
+                case "recent" -> {
+
+                    res = fs.saveResearchPapers(map);
+                }
+                case "experience" -> {
+                    res=fs.saveExperience(map);
+                }
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+        }
+        return new ResponseEntity<>(res==1?HttpStatus.CREATED:HttpStatus.CONFLICT);
     }
     @GetMapping("/faculty")
     ResponseEntity<Object> getAllFaculty() {
